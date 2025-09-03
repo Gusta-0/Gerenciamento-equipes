7dd5f5a1261ea965921ed73075c4f40d2297369f
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-09-03 13:36:50 -0300

    commit inicial

---
+++ src/main/java/com/ustore/gerenciamentoequipes/GerenciamentoEquipesApplication.java
@@ -0,0 +1,13 @@
+package com.ustore.gerenciamentoequipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEquipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoEquipesApplication.class, args);
+    }
+
+}
